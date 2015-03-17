29fb896f796dd173a2c21a29a565bd64830242b8
John Sheehan <jsheehan@jennasys.com>
2015-03-16 22:29:26 -0700

    Got image scraping working

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# image_project
-Group learning project
+image_project
+=========
+
+IEPUG Group learning project
+
+
+To install the module dependecies use the following (from within your virtual environment!):
+```
+pip install requests
+pip install beautifulsoup4
+pip install html5lib
+pip install exifread
+pip install sqlalchemy
+```
+
+
+Additional Info
+---------------
+In general, the overall idea of this sample project is to scrape a bunch of images off of flickr with something like [this](https://www.flickr.com/search/?q=california&cm=apple%2Fiphone_5s), extract some GPS EXIF info from the images, save the images to local storage, and some meta data to an sqlite database using SQLAlchemy.
+
+Additional steps will be to do some GIS tasks with the GPS info we saved, and then create a web app using flask.  After that we can put the web service in the cloud.
--- image_scrape.py
+++ image_scrape.py
@@ -3,26 +3,29 @@
 import requests
 from bs4 import BeautifulSoup
 from urlparse import urljoin
-import re
 import os
+
+#since we are ignoring SSL certificate errors with verify=False, we silence the important yet annoying log messages
+requests.packages.urllib3.disable_warnings()
 
 
 def get_image_page(url):
+    url = url  + 'sizes/o'  # grab the page for the original image size
     req = requests.get(url, verify=False)
     assert req.status_code == 200
 
     soup = BeautifulSoup(req.content)
 
-    for link in soup.find_all('img', 'main-photo'):
-        image_link = link.get('src')
-        imageList.append(image_link)
-        print image_link
-        # save_image(urljoin(req.request.url, image_link))
+    image_tag = soup.find('div',{'id':'allsizes-photo'}).find('img')
+    image_link = image_tag.get('src')
+    imageList.append(image_link)
+    print image_link
+    save_image(image_link)
 
 
 def save_image(url):
     SAVE_DIR = 'images'
-    req = requests.get(url)
+    req = requests.get(url, verify=False)
     fname = req.request.path_url.split('/')[-1]
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
@@ -42,13 +45,14 @@ def main():
     #get list of image URLs from search
     all_image_links = []
     for image_link in soup.find_all('a', {'class':'title'}):
+        # all_image_links.append(urljoin(req.request.url, image_link.get('href')))
         all_image_links.append(urljoin(req.request.url, image_link.get('href')))
     images = set(all_image_links)
 
     #download images and process exif info
     for image in images:
-        # get_image_page(image)
-        print image
+        get_image_page(image)
+        # print image
 
 
 if __name__ == "__main__":
