01c600a2d34d1bb54d2e2ba8d5ca3f8c8729c595
John Sheehan <jsheehan@jennasys.com>
2015-03-16 23:33:17 -0700

    Added the exif data extract

--- image_scrape.py
+++ image_scrape.py
@@ -4,13 +4,14 @@ import requests
 from bs4 import BeautifulSoup
 from urlparse import urljoin
 import os
+import exifread
 
 #since we are ignoring SSL certificate errors with verify=False, we silence the important yet annoying log messages
 requests.packages.urllib3.disable_warnings()
 
 
 def get_image_page(url):
-    url = url  + 'sizes/o'  # grab the page for the original image size
+    url += 'sizes/o'  # grab the page for the original image size instead of the generic image page
     req = requests.get(url, verify=False)
     assert req.status_code == 200
 
@@ -18,20 +19,42 @@ def get_image_page(url):
 
     image_tag = soup.find('div',{'id':'allsizes-photo'}).find('img')
     image_link = image_tag.get('src')
-    imageList.append(image_link)
-    print image_link
-    save_image(image_link)
+
+    if save_image(image_link):
+        imageList.append(image_link)
+        print image_link
 
 
 def save_image(url):
     SAVE_DIR = 'images'
     req = requests.get(url, verify=False)
     fname = req.request.path_url.split('/')[-1]
+
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
     with open(os.path.join(SAVE_DIR, fname), "wb") as content:
         content.write(req.content)
 
+    if get_exif_data(os.path.join(SAVE_DIR, fname)):
+        return True
+    else:
+        # No EXIF data available :-(
+        os.remove(os.path.join(SAVE_DIR, fname))
+        return False
+
+
+def get_exif_data(fname):
+
+    keys = ['GPS GPSLongitude', 'GPS GPSLatitude', 'EXIF ExifImageLength', 'EXIF ExifImageWidth', 'EXIF DateTimeOriginal']
+    with open(fname, 'r') as fimage:
+        tags = exifread.process_file(fimage)
+        if set(keys).issubset(tags):  # make sure all the EXIF tags we need are stored in the image
+            # we can write the EXIF data to the database here
+            for key in keys:
+                print "Key: %s, value %s" % (key, tags[key])
+            return True
+        else:
+            return False
 
 def main():
 
