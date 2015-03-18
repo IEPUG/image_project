98b05b7616c05fc0325babc31693d872b33b4073
Joshua Santos <nerdsville@nerdsville.net>
2015-03-18 19:42:38 -0400

    Minor changes to maintain PEP-8 compliance with the exceptance of line length, as well as converting to a class so it could be included in our flask API

--- image_scrape.py
+++ image_scrape.py
@@ -12,86 +12,96 @@ from sqlalchemy.orm import sessionmaker
 from table_def import Image
 
 
-#since we are ignoring SSL certificate errors with verify=False, we silence the important yet annoying log messages
-requests.packages.urllib3.disable_warnings()
+class ImageScraper:
 
+    # since we are ignoring SSL certificate errors with verify=False, we silence
+    # the important yet annoying log messages
+    requests.packages.urllib3.disable_warnings()
 
-def get_image_page(url):
-    url += 'sizes/o'  # grab the page for the original image size instead of the generic image page
-    req = requests.get(url, verify=False)
-    assert req.status_code == 200
+    def get_image_page(self, url):
+        # grab the page for the original image size instead
+        # the generic image page
+        url += 'sizes/o'
+        req = requests.get(url, verify=False)
+        assert req.status_code == 200
 
-    soup = BeautifulSoup(req.content)
+        soup = BeautifulSoup(req.content)
 
-    image_tag = soup.find('div',{'id':'allsizes-photo'}).find('img')
-    image_link = image_tag.get('src')
+        image_tag = soup.find('div', {'id': 'allsizes-photo'}).find('img')
+        image_link = image_tag.get('src')
 
-    if save_image(image_link):
-        imageList.append(image_link)
-        print image_link
+        if self.save_image(image_link):
+            imageList.append(image_link)
+            print image_link
 
+    def save_image(self, url):
+        SAVE_DIR = 'images'
+        req = requests.get(url, verify=False)
+        fname = req.request.path_url.split('/')[-1]
 
-def save_image(url):
-    SAVE_DIR = 'images'
-    req = requests.get(url, verify=False)
-    fname = req.request.path_url.split('/')[-1]
+        if not os.path.exists(SAVE_DIR):
+            os.makedirs(SAVE_DIR)
+        with open(os.path.join(SAVE_DIR, fname), "wb") as content:
+            content.write(req.content)
 
-    if not os.path.exists(SAVE_DIR):
-        os.makedirs(SAVE_DIR)
-    with open(os.path.join(SAVE_DIR, fname), "wb") as content:
-        content.write(req.content)
-
-    if get_exif_data(os.path.join(SAVE_DIR, fname), url):
-        return True
-    else:
-        # No EXIF data available :-(
-        os.remove(os.path.join(SAVE_DIR, fname))
-        return False
-
-
-def get_exif_data(fname, url):
-
-    keys = ['GPS GPSLongitude', 'GPS GPSLatitude', 'EXIF ExifImageLength', 'EXIF ExifImageWidth', 'EXIF DateTimeOriginal']
-    with open(fname, 'r') as fimage:
-        tags = exifread.process_file(fimage)
-        if set(keys).issubset(tags):  # make sure all the EXIF tags we need are stored in the image
-
-            image = Image(sourceUrl=url,
-              dateRetreived=datetime.datetime.now(),
-              latitude=str(tags['GPS GPSLatitude'].values),
-              longitude=str(tags['GPS GPSLongitude'].values),
-              imageDate=datetime.datetime.strptime(tags['EXIF DateTimeOriginal'].values,'%Y:%m:%d %H:%M:%S'),
-              imageHeight=tags['EXIF ExifImageLength'].values[0],
-              imageWidth=tags['EXIF ExifImageWidth'].values[0],
-              imageFile=url.split('/')[-1])
-
-            # Add the new record to the DB session object
-            session.add(image)
-
+        if self.get_exif_data(os.path.join(SAVE_DIR, fname), url):
             return True
         else:
+            # No EXIF data available :-(
+            os.remove(os.path.join(SAVE_DIR, fname))
             return False
 
-def main():
+    def get_exif_data(self, fname, url):
 
-    url = 'https://www.flickr.com/search/?q=california&cm=apple%2Fiphone_5s'
-    req = requests.get(url, verify=False)
+        keys = ['GPS GPSLongitude',
+                'GPS GPSLatitude',
+                'EXIF ExifImageLength',
+                'EXIF ExifImageWidth',
+                'EXIF DateTimeOriginal']
+        with open(fname, 'r') as fimage:
+            tags = exifread.process_file(fimage)
+            # make sure all the EXIF tags we need are stored in the image
+            if set(keys).issubset(tags):
+                image = Image(sourceUrl=url,
+                              dateRetreived=datetime.datetime.now(),
+                              latitude=str(tags['GPS GPSLatitude'].values),
+                              longitude=str(tags['GPS GPSLongitude'].values),
+                              imageDate=datetime.datetime.strptime(
+                                  tags['EXIF DateTimeOriginal'].values,
+                                  '%Y:%m:%d %H:%M:%S'
+                              ),
+                              imageHeight=tags['EXIF ExifImageLength'].values[0],
+                              imageWidth=tags['EXIF ExifImageWidth'].values[0],
+                              imageFile=url.split('/')[-1])
 
-    assert req.status_code == 200
+                # Add the new record to the DB session object
+                session.add(image)
 
-    soup = BeautifulSoup(req.content, "html5lib")
+                return True
+            else:
+                return False
 
-    #get list of image URLs from search
-    all_image_links = []
-    for image_link in soup.find_all('a', {'class':'title'}):
-        # all_image_links.append(urljoin(req.request.url, image_link.get('href')))
-        all_image_links.append(urljoin(req.request.url, image_link.get('href')))
-    images = set(all_image_links)
+    def main(self):
 
-    #download images and process exif info
-    for image in images:
-        get_image_page(image)
-        # print image
+        url = 'https://www.flickr.com/search/?q=california&cm=apple%2Fiphone_5s'
+        req = requests.get(url, verify=False)
+
+        assert req.status_code == 200
+
+        soup = BeautifulSoup(req.content, "html5lib")
+
+        # get list of image URLs from search
+        all_image_links = []
+        for image_link in soup.find_all('a', {'class': 'title'}):
+            # all_image_links.append(urljoin(req.request.url,
+            # image_link.get('href')))
+            all_image_links.append(urljoin(req.request.url, image_link.get('href')))
+        images = set(all_image_links)
+
+        # download images and process exif info
+        for image in images:
+            self.get_image_page(image)
+            # print image
 
 
 if __name__ == "__main__":
@@ -102,12 +112,11 @@ if __name__ == "__main__":
 
     imageList = []
     try:
-        main()
+        imageScraper = ImageScraper()
+        imageScraper.main()
         session.commit()
     finally:
-       # DB cleanup
+        # DB cleanup
         session.close()
 
     print('Image Count: {0}'.format(len(imageList)))
-
-
