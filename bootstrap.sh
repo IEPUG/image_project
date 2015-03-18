8cbcbffaee27a47af8950ad4e567205633d17284
Michael Albertson <mdalbertsonjr@gmail.com>
2015-03-17 19:14:55 -0700

    Commented out the git clone from the bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ apt-get install -y python-pip git python-virtualenv
 
 pip install requests beautifulsoup4 html5lib exifread sqlalchemy
 
-git clone https://github.com/IEPUG/image_project.git
+#git clone https://github.com/IEPUG/image_project.git
