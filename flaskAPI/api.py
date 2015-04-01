fe5af7061df660185d31af062fa720657bc82a4d
Joshua Santos <nerdsville@nerdsville.net>
2015-04-01 19:30:51 -0400

    Adding POST for more information backend functionality

--- flaskAPI/api.py
+++ flaskAPI/api.py
@@ -1,30 +1,34 @@
 from flask import Flask
-from flask.ext import restful
+from flask_restful import reqparse, Api, Resource
+from flask.json import JSONEncoder
 import queryImages
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = Api(app)
 
 
 @app.route("/")
 def hello():
     return "Hello from plain old flask"
 
-
-class APIRoot(restful.Resource):
+class APIRoot(Resource):
     def get(self):
         return {'context': 'api',
                 'message': 'Hello, World!'}
 
 api.add_resource(APIRoot, '/api/v1/')
 
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int, required=True,
+                    help="ID can not be blank!")
 
-class APIImages(restful.Resource):
+
+class APIImages(Resource):
     def __init__(self):
         self.imageReader = queryImages.ImageReader()
 
     def get(self):
-        self.imageReader.query()
+        self.imageReader.queryAll()
         results = []
         for i, result in enumerate(self.imageReader.results):
             resultDict = {
@@ -35,7 +39,21 @@ class APIImages(restful.Resource):
             results.append(resultDict)
         return {'images': results}
 
+    def post(self):
+        args = parser.parse_args()
+        self.imageReader.queryById(args["id"])
+        result = self.imageReader.imageInfo
+        resultDict = {
+            "sourceUrl": result.sourceUrl,
+            "dateRetreived": str(result.dateRetreived),
+            "imageDate": str(result.imageDate),
+            "imageWidth": result.imageWidth,
+            "imageHeight": result.imageHeight,
+            "imageFile": result.imageFile
+        }
+        return {args["id"]: resultDict}
 api.add_resource(APIImages, '/api/v1/images/')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
