97ac8fdef48069213c63c9f0cb574ce1427e8a36
Cameron Goodale <goodale@apache.org>
2015-04-01 22:18:13 -0700

    Start on the mapping application code
    *  Stubbed out a MapImage constructor
    *  Data parsing helper functions
    *  Create app object to namespace/contain application related code

--- www/js/main.js
+++ www/js/main.js
@@ -17,3 +17,70 @@ L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.pirates/{z}/{x}/{y}.png?acces
     maxZoom: 18
 }).addTo(map);
 
+
+
+
+// Image Object Constructor Function
+
+function MapImage(id, lat, lon) {
+	this.id = id;
+	this.lat = lat;
+	this.lon = lon;
+};
+
+function dmsToDecimalDegrees(deg, min, sec){
+	var decimalDegrees = deg + min/60 + sec/3600
+	return decimalDegrees
+}
+
+function convertStringGeoCoords(coords){
+	// expect coords of format "[33, 25, 7717/500]"
+	// trim off the [] values & split on the comma
+	var coordElements = coords.replace('[', '').replace(']', '').split(",");
+	// convert values to numbers
+	var degrees = new Number(coordElements[0]);
+	var minutes = new Number(coordElements[1]);
+
+	// should check IF rawSeconds contains '/' first
+	var rawSeconds = coordElements[2].split('/');
+	var numerator = new Number(rawSeconds[0]);
+	var denominator = new Number(rawSeconds[1]);
+	var seconds = numerator / denominator
+
+	var geoCoords = [degrees, minutes, seconds]
+	return geoCoords
+
+	// push onto the returned array
+}
+
+
+var app = {
+
+	images : [],
+
+	init : function(){
+
+		app.getAllImages();
+		app.dropMarkers();
+
+
+	},
+
+	dropMarkers : function(){
+		//Loop over the images array and insert a marker on the map
+	},
+
+	getAllImages : function() {
+		// GET url to fetch simple object with all images in an array
+		var url = "http://127.0.0.1:5000/api/v1/images/";
+		$.getJSON(url, function(data){
+			// Save Objects into the app.images array
+			for (var i=0; i < data.images.length; i++){
+
+				app.images.push(data.images[i]);
+			}
+			console.log("Loaded "+app.images.length+" from the server.");
+
+		});
+	}
+}
