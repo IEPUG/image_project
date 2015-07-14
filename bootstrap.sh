839a686197da69138e0f2066f4aa789872fb45a0
Larry Dennis <ronin@roninsa.com>
2015-07-14 13:14:47 -0700

    Removed bootstrap.sh & Vagrantfile

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y python-pip git python-virtualenv
-
-pip install requests beautifulsoup4 html5lib exifread sqlalchemy
-
-#git clone https://github.com/IEPUG/image_project.git
